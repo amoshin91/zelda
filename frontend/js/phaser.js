4df6dcbd1fe18f36e48ddd29270da5b86aff2f1e
github username <github email address>
2019-03-26 14:18:21 -0400

    coin collection - need to add coins to tiles

--- frontend/js/phaser.js
+++ frontend/js/phaser.js
@@ -17,41 +17,51 @@ const config = {
 };
 
 const game = new Phaser.Game(config);
-let rupeeScore = 0;
+// let score = 0;
 let score;
 let scoreText;
+let groundLayer;
 let gameOver = false
 
 
+
+////////////////////////////////////////////
+///////////////// PRELOAD //////////////////
+////////////////////////////////////////////
+
 function preload () {
 	// this.load.image('floor', 'js/assets/images/sprites/zelda/floor.jpg')
-  // this.load.image('tiles', 'js/assets/images/mountains.png')
-  this.load.image('walls', 'js/assets/images/walls.png')
-  this.load.image('floor', 'js/assets/images/tileset.png')
+   // this.load.image('tiles', 'js/assets/images/mountains.png')
+   this.load.image('walls', 'js/assets/images/walls.png')
+   this.load.image('floor', 'js/assets/images/tileset.png')
 	this.load.tilemapTiledJSON("map", 'js/assets/maps/dungeonnew.json')
-	this.load.spritesheet('link', 'js/assets/images/sprites/zelda/link-move-long-sheet.png',{ frameWidth: 24, frameHeight: 24});
+	this.load.spritesheet('link', 'js/assets/images/sprites/zelda/link-move-long-sheet.png', {frameWidth: 36, frameHeight: 36});
+   this.load.spritesheet('coin', 'js/assets/images/sprites/zelda/rupee.png', {frameWidth: 28, frameHeight: 28});
 
-}
+};
+
+////////////////////////////////////////////
+///////////////// CREATE ///////////////////
+////////////////////////////////////////////
 
 function create () {
-
-  // let scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' })
-  const map = this.make.tilemap({ key: "map" });
-  // const backgroundTileset = map.addTilesetImage("Wallpaper", 'walls')
-  const floorTileset = map.addTilesetImage("tileset", "floor");
-  const wallsTileset = map.addTilesetImage("walls", "walls");
+   // let scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' })
+   const map = this.make.tilemap({ key: "map" });
+   // const backgroundTileset = map.addTilesetImage("Wallpaper", 'walls')
+   const floorTileset = map.addTilesetImage("tileset", "floor");
+   const wallsTileset = map.addTilesetImage("walls", "walls");
 	const GroundLayer = map.createStaticLayer("Floors", floorTileset, 0, 0);
-  const BackgroundLayer = map.createStaticLayer("Walls", wallsTileset, 0, 0);
+   const BackgroundLayer = map.createStaticLayer("Walls", wallsTileset, 0, 0);
 
 	player = this.physics.add.sprite(400, 300, 'link');
-  enemy = this.physics.add.sprite(300, 200, 'link');
+   enemy = this.physics.add.sprite(300, 200, 'link');
 
 	cursors = this.input.keyboard.createCursorKeys();
 
-  this.anims.create({
-    key: 'stand',
-    frames: this.anims.generateFrameNumbers('link', {start: 0, end: 0}),
-  })
+   this.anims.create({
+      key: 'stand',
+      frames: this.anims.generateFrameNumbers('link', {start: 0, end: 0}),
+   })
 
   this.anims.create({
     key: 'left',
@@ -81,90 +91,42 @@ function create () {
     repeat: -1
 	});
 
-  player.setCollideWorldBounds(true);
-  this.physics.add.collider(player, enemy, this);
-  this.physics.add.collider(player, BackgroundLayer);
-
-
-   // coin = this.physics.add.sprite(500, 500, 'coin');
-   // console.log(coin);
-   // const coinTiles = map.addTilesetImage('coinTileName', 'tiles');
-   // const coinLayer = map.createDynamicLayer('coin', coinTiles, 0, 0);
-
-   this.coins = this.add.group();
-
-   let coins = this.add.sprite(300, 300, 'coin');
-   // this.coins.add(coin);
-
-
-   // coinLayer.setTileIndexCallback(26, collectcoin, this);
-   // this.physics.add.overlap(player, coinLayer);
-
-
-   this.anims.create({
-      key: 'stand',
-      frames: this.anims.generateFrameNumbers('link', {start: 0, end: 0}),
-   })
-
-   this.anims.create({
-      key: 'left',
-      frames: this.anims.generateFrameNumbers('link', {start: 1, end: 6}),
-      frameRate: 10,
-      repeat: -1
-   });
-
-   this.anims.create({
-      key: 'right',
-      frames: this.anims.generateFrameNumbers('link', {start: 7, end: 12}),
-      frameRate: 10,
-      repeat: -1
-   });
-
-   this.anims.create({
-      key: 'down',
-      frames: this.anims.generateFrameNumbers('link', {start: 21, end: 28}),
-      frameRate: 10,
-      repeat: -1
-   });
-
-   this.anims.create({
-      key: 'up',
-      frames: this.anims.generateFrameNumbers('link', {start: 13, end: 20}),
-      frameRate: 10,
-      repeat: -1
-   });
-
    this.anims.create({
       key: 'coin',
       frames: this.anims.generateFrameNumbers('coin', {start: 0, end: 9}),
       frameRate: 10,
       repeat: -1
-   })
+   });
 
-   // coinCollectText = this.add.text(20,570,'0', {
-   //    fontSize: '20px',
-   //    fill:'#fff'
-   // });
+   player.setCollideWorldBounds(true);
+   this.physics.add.collider(player, enemy, this);
+   this.physics.add.collider(player, BackgroundLayer);
 
-   // coinCollectText.setScrollFactor(0)
+   // let coins = this.add.sprite(300, 300, 'coin');
 
-   // let takeCoin = function(player, coin) {
-   //    coin.kill();
-   // }
-   //
-   // this.physics.overlap(player, coin);
+   const coinTiles = map.addTilesetImage('coin')
+   const coinLayer = map.createDynamicLayer('coin', coinTiles, 0, 0);
 
 
-   // takeCoin: function(player, coin) {
-   //    // coin.kill();
-   // };
+   // coinLayer.setTileIndexCallback(26, collectcoin, this);
+   // this.physics.add.overlap(player, coinLayer);
+
+   // this.physics.add.collider(groundLayer, player);
+
+
 
 
 
    player.setCollideWorldBounds(true);
 
    this.physics.add.overlap(player, coins, collectCoin, null, this);
-}
+};
+
+
+
+////////////////////////////////////////////
+///////////////// UPDATE ///////////////////
+////////////////////////////////////////////
 
 function update () {
    let linkSpeed = 3;
@@ -197,9 +159,12 @@ function update () {
    //    debugger
    // }
 
-}
+};
 
-function collectCoin(player, coin) {
+function collectCoin(sprite, tile) {
    // coin.disableBody(true, true);
+   coinLayer.removeTileAt(tile.x, tile.y);
+   score += 10;
    console.log('hi');
-}
+   return false;
+};

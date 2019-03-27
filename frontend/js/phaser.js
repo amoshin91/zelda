9612d8c81884e13f81ad4166ced27fc8398fdd07
github username <github email address>
2019-03-27 14:12:46 -0400

    tiles

--- frontend/js/phaser.js
+++ frontend/js/phaser.js
@@ -10,9 +10,9 @@ const config = {
 		}
   },
   scene: {
-    preload: preload,
-    create: create,
-    update: update
+	 preload: preload,
+	 create: create,
+	 update: update
   }
 };
 
@@ -29,8 +29,13 @@ function preload () {
   this.load.image('walls', 'js/assets/images/walls.png')
   this.load.image('floor', 'js/assets/images/tileset.png')
 	this.load.tilemapTiledJSON("map", 'js/assets/maps/dungeonnew.json')
-	this.load.spritesheet('link', 'js/assets/images/sprites/zelda/link-move-long-sheet.png',{ frameWidth: 30, frameHeight: 36});
-   this.load.spritesheet('gano', 'js/assets/images/sprites/zelda/ganondorf-move-sheet.png',{ frameWidth: 42, frameHeight: 42});
+
+	// this.load.image('tiles', 'js/assets/maps2/dungeon_tiles_0.png');
+	// this.load.tilemapTiledJSON('map', 'js/assets/maps2/dungeon.json');
+
+	this.load.spritesheet('link', 'js/assets/images/sprites/zelda/link-move-long-sheet.png', {frameWidth: 30, frameHeight: 36});
+	this.load.spritesheet('gano', 'js/assets/images/sprites/zelda/ganondorf-move-sheet.png', {frameWidth: 42, frameHeight: 42});
+	this.load.spritesheet('gano', 'js/assets/images/sprites/zelda/ganondorf-move-horizontal-sheet.png', {frameWidth: 30, frameHeigh: 42});
 
 
 }
@@ -44,8 +49,20 @@ function create () {
   // const backgroundTileset = map.addTilesetImage("Wallpaper", 'walls')
   const floorTileset = map.addTilesetImage("tileset", "floor");
   const wallsTileset = map.addTilesetImage("walls", "walls");
+
+	const tileset = map.addTilesetImage('walls', 'tiles')
+
 	const GroundLayer = map.createStaticLayer("Floors", floorTileset, 0, 0);
   const BackgroundLayer = map.createStaticLayer("Walls", wallsTileset, 0, 0);
+
+	BackgroundLayer.setCollisionBetween(170)
+
+
+	// const tileset = map.addTilesetImage('dungeon_tiles_0', 'tiles')
+	//
+	// const belowLayer = map.createStaticLayer('Below Player', tileset, 0, 0);
+	// const worldLayer = map.createStaticLayer('World', tileset, 0, 0);
+
 
 	player = this.physics.add.sprite(400, 300, 'link');
   enemy = this.physics.add.sprite(300, 200, 'gano');
@@ -53,51 +70,51 @@ function create () {
 	cursors = this.input.keyboard.createCursorKeys();
 
   this.anims.create({
-    key: 'stand',
-    frames: this.anims.generateFrameNumbers('link', {start: 0, end: 0}),
+		key: 'stand',
+		frames: this.anims.generateFrameNumbers('link', {start: 0, end: 0}),
   })
 
   this.anims.create({
-    key: 'left',
-    frames: this.anims.generateFrameNumbers('link', {start: 1, end: 6}),
-    frameRate: 10,
-    repeat: -1
+		key: 'left',
+		frames: this.anims.generateFrameNumbers('link', {start: 1, end: 6}),
+		frameRate: 10,
+		repeat: -1
   });
 
   this.anims.create({
-    key: 'right',
-    frames: this.anims.generateFrameNumbers('link', {start: 7, end: 12}),
-    frameRate: 10,
-    repeat: -1
+		key: 'right',
+		frames: this.anims.generateFrameNumbers('link', {start: 7, end: 12}),
+		frameRate: 10,
+		repeat: -1
   });
 
   this.anims.create({
-    key: 'down',
-    frames: this.anims.generateFrameNumbers('link', {start: 21, end: 28}),
-    frameRate: 10,
-    repeat: -1
+		key: 'down',
+		frames: this.anims.generateFrameNumbers('link', {start: 21, end: 28}),
+		frameRate: 10,
+		repeat: -1
   });
 
   this.anims.create({
-    key: 'up',
-    frames: this.anims.generateFrameNumbers('link', {start: 13, end: 20}),
-    frameRate: 10,
-    repeat: -1
+		key: 'up',
+		frames: this.anims.generateFrameNumbers('link', {start: 13, end: 20}),
+		frameRate: 10,
+		repeat: -1
 	});
 
-   this.anims.create({
-     key: 'ganleft',
-     frames: this.anims.generateFrameNumbers('gano', {start: 2, end: 8}),
-     frameRate: 10,
-     repeat: -1
-   });
+	this.anims.create({
+		key: 'ganleft',
+		frames: this.anims.generateFrameNumbers('gano', {start: 2, end: 8}),
+		frameRate: 10,
+		repeat: -1
+	});
 
-   this.anims.create({
-     key: 'ganright',
-     frames: this.anims.generateFrameNumbers('gano', {start: 9, end: 15}),
-     frameRate: 10,
-     repeat: -1
-   });
+	this.anims.create({
+	  key: 'ganright',
+	  frames: this.anims.generateFrameNumbers('gano', {start: 9, end: 15}),
+	  frameRate: 10,
+	  repeat: -1
+	});
 
   // player.setCollideWorldBounds(true);
   this.physics.add.collider(player, enemy, this);
@@ -109,39 +126,39 @@ function create () {
 function update () {
 	let linkSpeed = 3;
   if (cursors.left.isDown) {
-    player.x -= linkSpeed;
-    player.anims.play('left', true);
+		player.x -= linkSpeed;
+		player.anims.play('left', true);
   } else if (cursors.right.isDown) {
-    player.x += linkSpeed;
-    player.anims.play('right', true);
+		player.x += linkSpeed;
+		player.anims.play('right', true);
   } else if (cursors.down.isDown) {
-      player.y += linkSpeed;
-      player.anims.play('down', true);
+		player.y += linkSpeed;
+		player.anims.play('down', true);
   } else if (cursors.up.isDown) {
-      player.y -= linkSpeed;
-      player.anims.play('up', true);
+		player.y -= linkSpeed;
+		player.anims.play('up', true);
   } else {
-    player.anims.play('stand', true)
+		player.anims.play('stand', true)
   };
 
 	this.physics.moveToObject(enemy, player, 100)
 
-   if (enemy.body.velocity.x < 0) {
-      enemy.anims.play('ganleft', true)
-   } else {
-      enemy.anims.play('ganright', true)
-   }
-   // this.physics.world.collide()
-   // else
-   // {
-   //    player.setVelocityX(0)
-   //    player.setVelocityY(0)
-   //    player.anims.pause();
-   // };
+	if (enemy.body.velocity.x < 0) {
+		enemy.anims.play('ganleft', true)
+	} else {
+		enemy.anims.play('ganright', true)
+	}
+	// this.physics.world.collide()
+	// else
+	// {
+	//    player.setVelocityX(0)
+	//    player.setVelocityY(0)
+	//    player.anims.pause();
+	// };
 
-   // if (player.x <= 3) {
-   //    // player.x === 5
-   //    debugger
-   // }
+	// if (player.x <= 3) {
+	//    // player.x === 5
+	//    debugger
+	// }
 
 }
